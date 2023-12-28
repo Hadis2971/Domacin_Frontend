a21c674ff595c4c6f6acff80a967c6cdfe248ae0
Hadis1990 <hsteammsteam@gmail.com>
2023-12-28 17:31:27 +0100

    implement carousel image switch

--- src/components/Product/Product.tsx
+++ src/components/Product/Product.tsx
@@ -1,30 +1,145 @@
+import { useState } from "react";
+
 import Image from "react-bootstrap/Image";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCaretLeft, faCaretRight } from "@fortawesome/free-solid-svg-icons";
+
+import { ProductProps, ImagesCarouselProps } from "./types";
 
 import "./Product.scss";
 
-export default function () {
+function ImagesCarousel({
+  images,
+  currentImage,
+  onHandleGetNextImage,
+  onHandleGetPreviousImage,
+  onSetCurrentImage,
+}: ImagesCarouselProps) {
+  if (!images || images.length < 3) return null;
+
+  return (
+    <div id="images-carousel-container">
+      <FontAwesomeIcon icon={faCaretLeft} onClick={onHandleGetPreviousImage} />
+      <div className="images-carousel">
+        {images.map((image, idx) => {
+          return (
+            <div
+              className="next-image"
+              key={idx}
+              onClick={() => onSetCurrentImage(idx)}
+            >
+              <Image
+                src={image}
+                className={currentImage === idx ? "active-image" : ""}
+              />
+            </div>
+          );
+        })}
+      </div>
+
+      <FontAwesomeIcon icon={faCaretRight} onClick={onHandleGetNextImage} />
+    </div>
+  );
+}
+
+export default function ({
+  id,
+  title,
+  description,
+  price,
+  stock,
+  quantity,
+  categories,
+  images,
+}: ProductProps) {
+  const [currentImage, setCurrentImage] = useState(0);
+
+  const handleGetNextImage = () => {
+    const nextCurrentImage =
+      currentImage >= images.length - 1 ? 0 : currentImage + 1;
+
+    setCurrentImage(nextCurrentImage);
+
+    handleAnimateNextImage(nextCurrentImage);
+  };
+
+  const handleGetPreviousImage = () => {
+    const nextCurrentImage =
+      currentImage >= images.length - 1 ? 0 : currentImage + 1;
+
+    setCurrentImage(nextCurrentImage);
+
+    handleAnimatePreviousImage(nextCurrentImage);
+  };
+
+  const handleAnimateNextImage = (nextCurrentImage: number) => {
+    const parent = document.querySelectorAll(".images-carousel")[id - 1];
+    const children = parent.getElementsByClassName("next-image");
+    const firstChild = children[0];
+
+    parent?.removeChild(children[0]);
+    parent?.appendChild(firstChild);
+
+    if (nextCurrentImage && nextCurrentImage % 2 === 0) {
+      for (let i = 0; i < children.length; i++) {
+        children[i].classList.add("next-image-animation");
+      }
+    } else {
+      for (let i = 0; i < children.length; i++) {
+        children[i].classList.remove("next-image-animation");
+      }
+    }
+  };
+
+  const handleAnimatePreviousImage = (nextCurrentImage: number) => {
+    const parent = document.querySelectorAll(".images-carousel")[id - 1];
+    const children = parent.getElementsByClassName("next-image");
+
+    const firstChild = children[0];
+    const secondChild = children[1];
+
+    if (nextCurrentImage && nextCurrentImage % 2 === 0) {
+      for (let i = 0; i < children.length; i++) {
+        children[i].classList.add("previous-image-animation");
+      }
+    } else {
+      for (let i = 0; i < children.length; i++) {
+        children[i].classList.remove("previous-image-animation");
+      }
+    }
+
+    parent?.appendChild(firstChild);
+    parent?.removeChild(children[0]);
+    parent?.insertBefore(secondChild, parent.childNodes[0]);
+  };
+
+  if (!images.length) return null;
+
   return (
     <div className="product-container">
-      <div className="image-container">
-        <Image src="https://juliemarieeats.com/wp-content/uploads/2023/01/Bakery-Style-Chocolate-Chip-Cookies-14-scaled.jpg" />
+      <div className="images-container">
+        <div className="current-image-container">
+          {images[currentImage] ? <Image src={images[currentImage]} /> : null}
+        </div>
+        <ImagesCarousel
+          images={images}
+          currentImage={currentImage}
+          onHandleGetNextImage={handleGetNextImage}
+          onHandleGetPreviousImage={handleGetPreviousImage}
+          onSetCurrentImage={setCurrentImage}
+        />
       </div>
       <div className="info-container">
-        <h3>Title Of Cookies</h3>
-        <div>sasas, sasasa, asasas, asasas</div>
-        <div className="price">17.99$</div>
-        <div className="description">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce tempor
-          urna risus, a placerat mauris venenatis non. Pellentesque dapibus,
-          massa vitae semper tincidunt, felis libero eleifend diam, ac rutrum
-          tellus enim eget mi. Aenean cursus efficitur eros, quis vestibulum
-          ligula suscipit in. Nam laoreet odio mi, nec porta mauris rhoncus eu.
-          Proin eget iaculis leo. Nulla mi ligula, vulputate vitae gravida nec,
-          placerat sed lorem. Pellentesque cursus ornare nisl, in suscipit mi
-          faucibus eu. Duis lacinia nunc ac est placerat, eu posuere sapien
-          bibendum. Curabitur rutrum odio et ante pretium lacinia. Suspendisse
-          risus ante, ultrices eu ex eu, mattis rutrum leo. Vestibulum rhoncus
-          semper est a mattis. Duis eleifend sollicitudin ex, ac euismod est.
-        </div>
+        <h3>{title}</h3>
+
+        <div className="price">{`Cijena: ${price}`}</div>
+        <div className="stock">{`Raspolozivost: ${stock}`}</div>
+        {categories && categories.length > 0 && (
+          <div className="category">{`Kategorije: ${categories?.join(
+            ","
+          )}`}</div>
+        )}
+        <div className="description">{description}</div>
       </div>
     </div>
   );
--- src/components/Product/types.ts
+++ src/components/Product/types.ts
@@ -1,9 +1,21 @@
+import { Dispatch, SetStateAction } from "react";
+
 export type ProductProps = {
+  id: number;
   title: string;
   description: string;
   price: number;
   stock?: number;
-  category?: number[];
-  recension: number;
-  quantity: number;
+  recension?: number;
+  quantity?: number;
+  images: string[];
+  categories?: number[];
+};
+
+export type ImagesCarouselProps = {
+  images?: any[];
+  currentImage: number;
+  onHandleGetNextImage: () => void;
+  onHandleGetPreviousImage: () => void;
+  onSetCurrentImage: Dispatch<SetStateAction<number>>;
 };
